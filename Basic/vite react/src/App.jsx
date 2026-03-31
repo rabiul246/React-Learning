a8817a55c0b6ea22cbd5b4a9986b809f5f9359ce
rabiul246 <hasanrabiul118@gmail.com>
2026-03-31 16:19:58 +0600

    Added

---
+++ Basic/vite react/src/App.jsx
@@ -0,0 +1,13 @@
+import Start from './start'
+
+function App() {
+
+
+  return (
+    <>
+    <Start/>Rabiul Hasan
+    </>
+  )
+}
+
+export default App
---
+++ Basic/vite react/src/start.jsx
@@ -0,0 +1,7 @@
+function Start(){
+    return(
+        <h3>Learning Started</h3>
+    )
+}
+
+export default Start
